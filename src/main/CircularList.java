56d38768ae62dcccb8cd299bd1eb62a95a31def1
nishi0993 <nishi0993@gmail.com>
2019-02-26 22:16:06 -0800

    override toString method

--- src/main/CircularList.java
+++ src/main/CircularList.java
@@ -29,4 +29,19 @@ public class CircularList {
         System.out.println(list);
     }
 
+    @Override
+    public String toString() {
+        StringBuilder response = new StringBuilder();
+        response.append("[");
+        Node temp =this.head;
+        while(temp!=null){
+            response.append(temp.data);
+            if(temp.next!=null){
+                response.append(" ==> ");
+            }
+            temp=temp.next;
+        }
+        response.append("]");
+        return response;
+    }
 }
